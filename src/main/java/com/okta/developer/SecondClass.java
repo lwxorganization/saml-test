403d11449af4e1e8db9a0d3e20bb67c70500750e
V·Sel <liuweixiao@liquido.cn>
2022-07-25 18:33:27 +0800

    合并3-4-5

--- src/main/java/com/okta/developer/SecondClass.java
+++ src/main/java/com/okta/developer/SecondClass.java
@@ -6,6 +6,6 @@ package com.okta.developer;
  */
 public class SecondClass {
     public static void main(String[] args) {
-
+        System.out.println("0-----");
     }
 }
--- src/main/java/com/okta/developer/ThirdClass.java
+++ src/main/java/com/okta/developer/ThirdClass.java
@@ -5,4 +5,7 @@ package com.okta.developer;
  * @date 2022/7/25
  */
 public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.println("=====>>>");
+    }
 }
